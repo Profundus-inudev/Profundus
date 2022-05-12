d61794bedf83640ba7c18c65355772bd53010fe9
tererun <27857171+tererun@users.noreply.github.com>
2022-05-12 18:23:02 +0900

    :zap: ConfigHandlerのインスタンスをメインクラスに追加

--- src/main/java/tech/inudev/metaverseplugin/Metaverseplugin.java
+++ src/main/java/tech/inudev/metaverseplugin/Metaverseplugin.java
@@ -2,17 +2,24 @@ package tech.inudev.metaverseplugin;
 
 import lombok.Getter;
 import org.bukkit.plugin.java.JavaPlugin;
+import tech.inudev.metaverseplugin.config.ConfigHandler;
 
 import java.util.logging.Logger;
 
 public final class Metaverseplugin extends JavaPlugin {
     @Getter
     private static Logger logger;
+    private static Metaverseplugin instance;
+
+    private ConfigHandler configHandler;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        instance = this;
         logger = getLogger();
+
+        this.configHandler = new ConfigHandler(instance);
 
         logger.info("Metaverseplugin enabled!");
     }
@@ -23,4 +30,13 @@ public final class Metaverseplugin extends JavaPlugin {
 
         logger.info("Metaverseplugin disabled!");
     }
+
+    public static Metaverseplugin getInstance() {
+        return instance;
+    }
+
+    public ConfigHandler getConfigHandler() {
+        return configHandler;
+    }
+
 }
