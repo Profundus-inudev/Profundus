dfe79bc5b9b5d2b1522d860b35d0de96fa3458bb
toru-toruto <toruto.torutoru.toruto@gmail.com>
2022-05-14 18:35:56 +0900

    Moneyクラスの実装

---
+++ src/main/java/tech/inudev/metaverseplugin/money/Money.java
@@ -0,0 +1,83 @@
+package tech.inudev.metaverseplugin.money;
+
+import lombok.Getter;
+import tech.inudev.metaverseplugin.Metaverseplugin;
+
+/**
+ * お金の取引を処理するためのクラス
+ *
+ * @author toru-toruto
+ */
+public class Money {
+    @Getter
+    private long amount;
+    private int playerUUID;
+    private String bankName;
+
+    /**
+     * プレイヤーの所持金を使用する場合のコンストラクタ
+     * @param playerUUID プレイヤーのUUID
+     */
+    public Money(int playerUUID) {
+        init(playerUUID, "");
+    }
+
+    /**
+     * プレイヤーの口座上のお金を使用する場合のコンストラクタ
+     * @param playerUUID プレイヤーのUUID
+     * @param bankName 口座の名前
+     */
+    public Money(int playerUUID, String bankName) {
+        init(playerUUID, bankName);
+    }
+
+
+    private void init (int playerUUID, String bankName) {
+        long mock = bankName.isEmpty()
+            ? 1000 // データベースよりプレイヤ(playerUUIDの所持金の取得
+            : 100000; // データベースよりプレイヤ(playerUUID)の口座(bankName)上の金額の取得
+
+        this.amount = mock;
+        this.playerUUID = playerUUID;
+        this.bankName = bankName;
+    }
+
+    /**
+     * 金額への加算
+     * @param value 加算する金額
+     */
+    public void add(long value) {
+        this.amount += value;
+    }
+
+    /**
+     * 金額への減算
+     * @param value 減算する金額
+     */
+    public void remove(long value) {
+        if (this.amount < value) {
+            logging("取引するためのお金が足りません");
+        } else {
+            this.amount -= value;
+        }
+    }
+
+    /**
+     * 取引後の金額をDatabaseへ反映する
+     */
+    public void push() {
+        if (isBankMoney()) {
+            logging("取引後の口座の金額をDatabaseへ反映します");
+        } else {
+            logging("取引後の所持金をDatabaseへ反映します");
+        }
+    }
+
+    private boolean isBankMoney() {
+        return bankName.isEmpty();
+    }
+
+    private void logging(String message) {
+        Metaverseplugin.getInstance().getLogger().info(message);
+    }
+}
