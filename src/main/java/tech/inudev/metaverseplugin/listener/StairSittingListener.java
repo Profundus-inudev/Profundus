bb2c7ab8a4737352c67b351060ff70d745273262
toru-toruto <toruto.torutoru.toruto@gmail.com>
2022-05-26 05:10:24 +0900

    整理：リファクタリング

--- src/main/java/tech/inudev/metaverseplugin/listener/StairSittingListener.java
+++ src/main/java/tech/inudev/metaverseplugin/listener/StairSittingListener.java
@@ -20,46 +20,58 @@ public class StairSittingListener implements Listener {
     @EventHandler
     public void onPlayerInteractEvent(PlayerInteractEvent e) {
         Block stair = e.getClickedBlock();
-        if (e.getAction() != Action.RIGHT_CLICK_BLOCK
-                || stair == null
-                || stair.getType() != Material.OAK_STAIRS) {
-            // 指定の階段ブロックでなければ座れない
+        if (stair == null
+                || !isTargetStair(stair, e.getAction())
+                || !hasEnoughSpace(stair)) {
             return;
         }
+        e.setCancelled(true);
+        LivingEntity seatEntity = createSeatEntity(e.getPlayer(), getSeatLocation(stair));
+        seatEntity.addPassenger(e.getPlayer());
+    }
 
-        Bisected.Half dir =  ((Stairs) stair.getBlockData()).getHalf();
+    private boolean isTargetStair(Block stair, Action action) {
+        // 指定の階段ブロックでなければ座れない
+        return action == Action.RIGHT_CLICK_BLOCK
+                && stair.getType() == Material.OAK_STAIRS;
+    }
+
+    private boolean hasEnoughSpace(Block stair) {
+        Bisected.Half dir = ((Stairs) stair.getBlockData()).getHalf();
         if (dir == Bisected.Half.TOP) {
             // 階段ブロックが逆さならば座れない
-            return;
+            return false;
         }
 
         Location upLoc = stair.getLocation().clone().add(0, 1.0, 0);
         if (upLoc.getBlock().getType() != Material.AIR) {
             // 上が開いてなければ座れない
-            return;
+            return false;
         }
+        return true;
+    }
 
-        e.setCancelled(true);
-
+    private Location getSeatLocation(Block stair) {
         // 階段ブロックの中心位置へ（すこしだけ下へ）
         Vector offsetToCenter = new Vector(0.5, -0.4, 0.5);
         // 階段ブロック中心から少しだけ前方へ
         double forwardOffset = 0.2;
-        Vector stairDir = ((Stairs) stair.getBlockData()).getFacing().getDirection();
 
-        Location seatEntityLoc = stair.getLocation().clone();
+        Location seatLoc = stair.getLocation().clone();
+        // 階段ブロックは椅子の背もたれ方向が前方
+        Vector seatDir = ((Stairs) stair.getBlockData()).getFacing().getDirection().clone();
+        seatDir = new Vector(-seatDir.getX(), 0, -seatDir.getZ());
 
-//        e.getPlayer().sendMessage(Component.text("($x, $y, $z)"
-//                .replace("$x", "" + (offsetToCenter.getX() - forwardOffset * stairDir.getX()))
-//                .replace("$y", "" + (offsetToCenter.getY() - forwardOffset * stairDir.getY()))
-//                .replace("$z", "" + (offsetToCenter.getZ() - forwardOffset * stairDir.getZ()))));
+        seatLoc.add(
+                offsetToCenter.getX() + forwardOffset * seatDir.getX(),
+                offsetToCenter.getY() + forwardOffset * seatDir.getY(),
+                offsetToCenter.getZ() + forwardOffset * seatDir.getZ());
+        seatLoc.setDirection(seatDir);
+        return seatLoc;
+    }
 
-        seatEntityLoc.add(
-                offsetToCenter.getX() - forwardOffset * stairDir.getX(),
-                offsetToCenter.getY() - forwardOffset * stairDir.getY(),
-                offsetToCenter.getZ() - forwardOffset * stairDir.getZ());
-
-        Bat bat = (Bat) e.getPlayer().getWorld().spawnEntity(seatEntityLoc, EntityType.BAT);
+    private LivingEntity createSeatEntity(Player player, Location seatLoc) {
+        Bat bat = (Bat) player.getWorld().spawnEntity(seatLoc, EntityType.BAT);
         Objects.requireNonNull(bat.getAttribute(Attribute.GENERIC_MAX_HEALTH)).setBaseValue(1);
         bat.setSilent(true);
         bat.setInvisible(true);
@@ -67,10 +79,7 @@ public class StairSittingListener implements Listener {
         bat.setInvulnerable(true);
         bat.setAwake(true);
         bat.setAI(false);
-        bat.teleport(bat.getLocation().setDirection(
-                new Vector(-stairDir.getX(), 0, -stairDir.getZ())));
-
-        bat.addPassenger(e.getPlayer());
+        return bat;
     }
 
     @EventHandler
