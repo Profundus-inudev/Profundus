2e9ed444f01dc791ab4b8d639c2cfb038234c07c
toru-toruto <toruto.torutoru.toruto@gmail.com>
2022-06-12 00:36:15 +0900

    整形：リファクタリング

--- src/main/java/tech/inudev/profundus/utils/HelpUtil.java
+++ src/main/java/tech/inudev/profundus/utils/HelpUtil.java
@@ -11,14 +11,11 @@ import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.map.MinecraftFont;
 import tech.inudev.profundus.Profundus;
 
-import java.awt.print.Book;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -32,6 +29,7 @@ public class HelpUtil {
                         .author(Component.text("Master"))
                         .title(Component.text("Help"));
 
+                // テキストファイルからの読み込み
                 InputStream stream = Profundus.getInstance().getResource("help/test.txt");
                 if (stream == null) {
                     player.sendMessage(Component.text("error: opening help failed."));
@@ -40,12 +38,8 @@ public class HelpUtil {
                 InputStreamReader reader = new InputStreamReader(stream, StandardCharsets.UTF_8);
                 Stream<String> lines = new BufferedReader(reader).lines();
                 String str = lines.collect(Collectors.joining("\n"));
-//                String str = """
-//                        これから説明しますのは、なんといってもowesomeでgreatなsomething。
-//
-//                         まず第一にclass 1において、この機構は軸のroleを果たしている。一見すると取るに足らないcrude shapeをしているが、実際のところ、これがthe best shapeであることは間違いない。
-//
-//                        ごま豆乳鍋のprecious valueは、この舌触りにある。通常の鍋は、どちらかと言えば澄ましたうまみ出しによる鍋であるが、ごま豆乳鍋では豆乳の滑らかさに加え、ごまのほのかなざらつきによって、深みのある食感体験を演出しているのである。""";
+
+                // 本に合わせて整形
                 List<Component> pageList = new ArrayList<>();
                 List<String> bookLines = HelpUtil.getLines(str);
                 String page = "";
@@ -57,7 +51,8 @@ public class HelpUtil {
                         page = "";
                     }
                 }
-                bookMeta.addPages(pageList.toArray(new Component[pageList.size()]));
+
+                bookMeta.addPages(pageList.toArray(new Component[0]));
                 writtenBook.setItemMeta(bookMeta);
             }
             player.openBook(writtenBook);
@@ -68,103 +63,89 @@ public class HelpUtil {
         Profundus.getInstance().getLogger().info(str);
     }
 
-    public static List<String> getLines(String text) {
+    private static List<String> getLines(String text) {
         final MinecraftFont font = new MinecraftFont();
         final int maxLineWidth = font.getWidth("LLLLLLLLLLLLLLLLLLL");
-//        logging("max: " + maxLineWidth);
+        final int charMargin = 1;
+
+        // それぞれの行について処理
         List<String> lineList = new ArrayList<>();
         text.lines().forEach((String section) -> {
             if (section.equals("")) {
+                // 空行の場合
                 lineList.add("");
             } else {
-                String[] wordList = ChatColor.stripColor(section).split(" ");
-                String line = ""; // 行の文字列を保存
-                int width = 0;
-                for (String word : wordList) {
+                // 行を半角スペースごとに分割し、それぞれの文字列を処理（主に英単語をまとめて折り返す目的）
+                final String rawSection = ChatColor.stripColor(section);
+                StringBuilder lineStr = new StringBuilder(); // 行の文字列を保存
+                int lineWidth = 0;
+                for (String word : rawSection.split(" ")) {
                     if (word.equals("")) {
-                        line += " ";
+                        lineStr.append(" ");
                         continue;
                     }
-                    // 分割された文字列を文字単位に
+                    // 文字列を文字単位に分割し、それぞれ処理
                     String[] charList = word.split("");
                     int i = 0;
                     while (i < charList.length) {
                         if (font.isValid(charList[i])) {
-                            // MinecraftFontに存在する文字の場合、つまりwidthが定義されている場合
-//                            logging(charList[i] + ": " + font.getWidth(charList[i]));
-                            String mcFontWord = charList[i];
-                            // MinecraftFontに含まれる文字が続く限りiを進行
-                            while (i < word.length() - 1 && font.isValid(charList[i + 1])) {
-//                                logging(charList[i + 1] + ": " + font.getWidth(charList[i+1]));
+                            // MinecraftFontに文字が定義されている場合
+                            StringBuilder mcFontWord = new StringBuilder(charList[i]);
 
-                                mcFontWord += charList[i + 1];
+                            // 連続するMinecraftFontに定義される文字をまとめて処理
+                            while (i < word.length() - 1 && font.isValid(charList[i + 1])) {
+                                mcFontWord.append(charList[i + 1]);
                                 i++;
                             }
-                            // 主に英単語の幅が最大を超えてしまう場合、英単語ごと折り返す
-                            if (width + (line.equals("") ? 0 : 1) + font.getWidth(mcFontWord) > maxLineWidth) {
+
+                            // 追加される（主に）英単語がはみ出す場合、英単語ごと折り返す
+                            final int newWidth = (lineStr.toString().equals("") ? 0 : charMargin)
+                                    + font.getWidth(mcFontWord.toString());
+                            if (lineWidth + newWidth > maxLineWidth) {
                                 // todo:ひと単語で一行埋めてしまう場合の特別処理が必要
-                                lineList.add(line);
-                                // 次の行
-                                width = 0;
-                                line = "";
+                                lineList.add(lineStr.toString());
+                                // 次の行へ
+                                lineWidth = 0;
+                                lineStr = new StringBuilder();
                             }
-                            width += line.equals("")
-                                    ? font.getWidth(mcFontWord)
-                                    : font.getWidth(mcFontWord) + 1;
-                            line += mcFontWord;
-                            i++;
-                            if (width + 1 + font.getWidth(" ") <= maxLineWidth && i == word.length()) {
-                                line += " ";
-                                width += 1 + font.getWidth(" ");
+
+                            // 行へ追加
+                            lineWidth += lineStr.toString().equals("")
+                                    ? font.getWidth(mcFontWord.toString())
+                                    : charMargin + font.getWidth(mcFontWord.toString());
+                            lineStr.append(mcFontWord);
+
+                            // 行のラストにスペースの余地があるならスペースを入れる
+                            final int endSpaceWidth = charMargin + font.getWidth(" ");
+                            if (i == word.length() - 1 && lineWidth + endSpaceWidth <= maxLineWidth) {
+                                lineWidth += endSpaceWidth;
+                                lineStr.append(" ");
                             }
                         } else {
-                            final int margin = 1;
+                            // MinecraftFontに文字が定義されていない場合（日本語やその他の文字）
                             final int charWidth = 8; // 全角文字の幅基準
-                            logging(charList[i] + ": " + charWidth);
 
-//                            // 日本語の場合
-//                            if (isJapanese(charList[i])) {
-//                                final int margin = 1;
-//                                final int jpCharWidth = 8;
-//                                String jpWord = "";
-//                                // 日本語文字が続く限りiを進行
-//                                while(i < word.length() - 1 && isJapanese(charList[i + 1])) {
-//                                    int currentWidth = (jpCharWidth * jpWord.length())
-//                                            + (margin * (jpWord.length() - 1));
-//
-//                                    // 日本語が続くが文字列の幅が最大を超えてしまう場合
-//                                    if (currentWidth + margin + jpCharWidth > maxLineWidth) {
-//                                        line += jpWord;
-//                                        lineList.add(line);
-//                                        line = "";
-//                                        jpWord = "";
-//                                    }
-//                                    jpWord += charList[i + 1];
-//                                    i++;
-//                                }
-                            if (width + margin + charWidth > maxLineWidth) {
-                                lineList.add(line);
-                                // 次の行
-                                width = 0;
-                                line = "";
+                            // 追加される文字がはみ出す場合、折り返す
+                            final int newWidth = charMargin + charWidth;
+                            if (lineWidth + newWidth > maxLineWidth) {
+                                lineList.add(lineStr.toString());
+                                // 次の行へ
+                                lineWidth = 0;
+                                lineStr = new StringBuilder();
                             }
-                            width += (line.equals(""))
+
+                            // 行へ追加
+                            lineWidth += (lineStr.toString().equals(""))
                                     ? charWidth
-                                    : margin + charWidth;
-                            line += charList[i];
-                            i++;
+                                    : charMargin + charWidth;
+                            lineStr.append(charList[i]);
                         }
+                        i++;
                     }
                 }
-                lineList.add(line);
+                lineList.add(lineStr.toString());
             }
         });
         return lineList;
     }
-
-//    private static boolean isJapanese(String str) {
-//        String regex = "[　一-龠ぁ-んァ-ヶ]+";
-//        return str.matches(regex);
-//    }
-
 }
