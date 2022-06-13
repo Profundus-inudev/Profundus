b68af5c95a19d6f028950b38cdedee29cb9471cc
toru-toruto <toruto.torutoru.toruto@gmail.com>
2022-06-13 23:37:19 +0900

    整形：リファクタリング

--- src/main/java/tech/inudev/profundus/utils/HelpUtil.java
+++ src/main/java/tech/inudev/profundus/utils/HelpUtil.java
@@ -94,107 +94,206 @@ public class HelpUtil {
      * @return 本のサイズに合わせて整形されたテキストの行ごとのリスト
      */
     private static List<String> getLines(String text) {
-        final MinecraftFont font = new MinecraftFont();
-        final int maxLineWidth = font.getWidth("LLLLLLLLLLLLLLLLLLL");
-        final int charMargin = 1;
 
         List<String> resultLines = new ArrayList<>();
 
         // それぞれの行についてループ
-        for (String section : text.lines().toList()) {
-            if (section.equals("")) {
+        for (String paragraph : text.lines().toList()) {
+            if (paragraph.equals("")) {
                 // 空行の場合
                 resultLines.add("");
-                continue;
             } else {
-                StringBuilder newLineStr = new StringBuilder();
-                int newLineWidth = 0;
-
-                // 半角スペースで分割した各文字列をループ（主に英単語をまとめて折り返す目的）
-                for (String word : section.split(" ")) {
-                    if (word.equals("")) {
-                        // 文頭や連続半角スペースの場合
-                        newLineStr.append(" ");
-                        continue;
-                    }
-                    // 文字列を文字単位に分割しループ
-                    String[] charList = word.split("");
-                    int i = 0;
-                    while (i < charList.length) {
-                        if (i < charList.length - 1 && isColorWord(charList[i], charList[i + 1])) {
-                            // カラーコードまたは装飾コードの場合
-                            if (!charList[i + 1].matches("[lL]")) {
-                                // 文字幅が変化すると困るのでボールドの装飾は解除
-                                newLineStr.append(charList[i]).append(charList[i + 1]);
-                            }
-                            i += 2;
-                            continue;
-                        } else if (font.isValid(charList[i])) {
-                            // MinecraftFontに文字が定義されている場合
-                            StringBuilder newWord = new StringBuilder(charList[i]);
-
-                            // 連続するMinecraftFontに定義される文字をまとめて処理
-                            while (i < word.length() - 1 && font.isValid(charList[i + 1])) {
-                                newWord.append(charList[i + 1]);
-                                i++;
-                            }
-                            // 追加される（主に）英単語がはみ出す場合、英単語ごと折り返す
-                            final int newWidth = (newLineStr.toString().equals("") ? 0 : charMargin)
-                                    + font.getWidth(newWord.toString());
-                            if (newLineWidth + newWidth > maxLineWidth) {
-                                // todo:ひと単語で一行埋めてしまう場合の特別処理が必要
-                                resultLines.add(newLineStr.toString());
-                                // 次の行へ
-                                newLineWidth = 0;
-                                newLineStr = new StringBuilder();
-                            }
-
-                            // 行へ追加
-                            newLineWidth += newLineStr.toString().equals("")
-                                    ? font.getWidth(newWord.toString())
-                                    : charMargin + font.getWidth(newWord.toString());
-                            newLineStr.append(newWord);
-
-                            // 行のラストにスペースの余地があるならスペースを入れる
-                            final int endSpaceWidth = charMargin + font.getWidth(" ");
-                            if (i == word.length() - 1 && newLineWidth + endSpaceWidth <= maxLineWidth) {
-                                newLineWidth += endSpaceWidth;
-                                newLineStr.append(" ");
-                            }
-                            i++;
-                            continue;
-                        } else {
-                            // MinecraftFontに文字が定義されていない場合（日本語やその他の文字）
-                            final int charWidth = 8; // 全角文字の幅基準
-
-                            // 追加される文字がはみ出す場合、折り返す
-                            final int newWidth = charMargin + charWidth;
-                            if (newLineWidth + newWidth > maxLineWidth) {
-                                resultLines.add(newLineStr.toString());
-                                // 次の行へ
-                                newLineWidth = 0;
-                                newLineStr = new StringBuilder();
-                            }
-
-                            // 行へ追加
-                            newLineWidth += (newLineStr.toString().equals(""))
-                                    ? charWidth
-                                    : charMargin + charWidth;
-                            newLineStr.append(charList[i]);
-                            i++;
-                            continue;
-                        }
-                    }
-                }
-                resultLines.add(newLineStr.toString());
+                resultLines.addAll(buildLines(paragraph));
             }
         }
         return resultLines;
     }
 
-    private static boolean isColorWord(String str0, String str1) {
+    private static List<String> buildLines(String paragraph) {
+        final List<String> newLines = new ArrayList<>();
+
+        final MinecraftFont font = new MinecraftFont();
+
+        StringBuilder newLineBuilder = new StringBuilder();
+        int newLineWidth = 0;
+
+        // 半角スペースで分割した各文字列をループ（主に英単語をまとめて折り返す目的）
+        for (String word : paragraph.split(" ")) {
+            if (word.equals("")) {
+                // 文頭や連続半角スペースの場合
+                newLineBuilder.append(" ");
+            } else {
+                JoinWordResult result =
+                        joinWord(newLineBuilder.toString(), newLineWidth, word, font);
+                newLines.addAll(result.newLines);
+                newLineBuilder = new StringBuilder(result.newLineStr);
+                newLineWidth = result.newLineWidth;
+            }
+        }
+        if (!newLineBuilder.toString().equals("")) {
+            newLines.add(newLineBuilder.toString());
+        }
+//        resultLines.add(newLineBuilder);
+        return newLines;
+    }
+
+    private static class JoinWordResult {
+        List<String> newLines;
+        String newLineStr;
+        int newLineWidth;
+
+        JoinWordResult(List<String> newLines, String newLineStr, int newLineWidth) {
+            this.newLines = newLines;
+            this.newLineStr = newLineStr;
+            this.newLineWidth = newLineWidth;
+        }
+    }
+
+    private static JoinWordResult joinWord(String lineStr, int newLineWidth, String word, MinecraftFont font) {
+        List<String> newLines = new ArrayList<>();
+        StringBuilder newLineStr = new StringBuilder(lineStr);
+
+        // 文字列を文字単位に分割しループ
+        String[] letters = word.split("");
+        int id = 0;
+        while (id < letters.length) {
+            if (isSectionLetter(letters[id])) {
+                DecorationResult result
+                        = joinDecorationLetters(newLineStr.toString(), letters, id);
+                newLineStr = new StringBuilder(result.newLineStr);
+                id = result.id;
+            } else {
+                final int maxLineWidth = font.getWidth("LLLLLLLLLLLLLLLLLLL");
+                final int letterMargin = 1;
+                JoinLetterResult result = font.isValid(letters[id])
+                        ? joinRegisteredLetters(newLineStr.toString(), letters, id, newLineWidth, font, maxLineWidth, letterMargin)
+                        : joinUnregisteredLetter(newLineStr.toString(), letters, id, newLineWidth, maxLineWidth, letterMargin);
+                if (result.newLines.size() > 0) {
+                    newLines.addAll(result.newLines);
+                }
+                newLineStr = new StringBuilder(result.newLineStr);
+                id = result.id;
+                newLineWidth = result.newLineWidth;
+            }
+        }
+        return new JoinWordResult(newLines, newLineStr.toString(), newLineWidth);
+    }
+
+    private static class DecorationResult {
+        String newLineStr;
+        int id;
+
+        DecorationResult(String newLineStr, int id) {
+            this.newLineStr = newLineStr;
+            this.id = id;
+        }
+    }
+
+    private static DecorationResult joinDecorationLetters(String lineStr, String[] letters, int id) {
+        StringBuilder newLineStr = new StringBuilder(lineStr);
+        if (id < letters.length - 1 && isDecorationLetter(letters[id + 1])) {
+            if (!letters[id + 1].matches("[lL]")) {
+                newLineStr.append(letters[id]).append(letters[id + 1]);
+            }
+            id += 2;
+        } else {
+            id++;
+        }
+        return new DecorationResult(newLineStr.toString(), id);
+    }
+
+    private static class JoinLetterResult {
+        List<String> newLines;
+        String newLineStr;
+        int id;
+        int newLineWidth;
+
+        JoinLetterResult(List<String> newLines, String newLineStr, int id, int newLineWidth) {
+            this.newLines = newLines;
+            this.newLineStr = newLineStr;
+            this.id = id;
+            this.newLineWidth = newLineWidth;
+        }
+    }
+
+    private static JoinLetterResult joinRegisteredLetters(
+            String lineStr,
+            String[] letters, int id, int newLineWidth,
+            MinecraftFont font, int maxLineWidth, int letterMargin) {
+        // MinecraftFontに文字が定義されている場合
+        List<String> newLines = new ArrayList<>();
+        StringBuilder newLineStr = new StringBuilder(lineStr);
+
+        StringBuilder newWord = new StringBuilder(letters[id]);
+
+        // 連続するMinecraftFontに定義される文字をまとめて処理
+        while (id < letters.length - 1 && font.isValid(letters[id + 1])) {
+            newWord.append(letters[id + 1]);
+            id++;
+        }
+        // 追加される（主に）英単語がはみ出す場合、英単語ごと折り返す
+        final int newWidth = (newLineStr.toString().equals("") ? 0 : letterMargin)
+                + font.getWidth(newWord.toString());
+        if (newLineWidth + newWidth > maxLineWidth) {
+            // todo:ひと単語で一行埋めてしまう場合の特別処理が必要
+//            resultLines.add(newLineStr.toString());
+            newLines.add(newLineStr.toString());
+            // 次の行へ
+            newLineWidth = 0;
+            newLineStr = new StringBuilder();
+        }
+
+        // 行へ追加
+        newLineWidth += newLineStr.toString().equals("")
+                ? font.getWidth(newWord.toString())
+                : letterMargin + font.getWidth(newWord.toString());
+        newLineStr.append(newWord);
+
+        // 行のラストにスペースの余地があるならスペースを入れる
+        final int endSpaceWidth = letterMargin + font.getWidth(" ");
+        if (id == letters.length - 1 && newLineWidth + endSpaceWidth <= maxLineWidth) {
+            newLineWidth += endSpaceWidth;
+            newLineStr.append(" ");
+        }
+        id++;
+        return new JoinLetterResult(newLines, newLineStr.toString(), id, newLineWidth);
+    }
+
+    private static JoinLetterResult joinUnregisteredLetter(
+            String lineStr,
+            String[] letters, int id, int newLineWidth,
+            int maxLineWidth, int letterMargin) {
+        // MinecraftFontに文字が定義されていない場合（日本語やその他の文字）
+        final int letterWidth = 8; // 全角文字の幅基準
+        List<String> newLines = new ArrayList<>();
+
+        StringBuilder newLineStr = new StringBuilder(lineStr);
+
+        // 追加される文字がはみ出す場合、折り返す
+        final int newWidth = letterMargin + letterWidth;
+        if (newLineWidth + newWidth > maxLineWidth) {
+//            resultLines.add(newLineStr.toString());
+            newLines.add(newLineStr.toString());
+            // 次の行へ
+            newLineWidth = 0;
+            newLineStr = new StringBuilder();
+        }
+
+        // 行へ追加
+        newLineWidth += (newLineStr.toString().equals(""))
+                ? letterWidth
+                : letterMargin + letterWidth;
+        newLineStr.append(letters[id]);
+        id++;
+        return new JoinLetterResult(newLines, newLineStr.toString(), id, newLineWidth);
+    }
+
+    private static boolean isSectionLetter(String letter) {
+        return letter.equals(String.valueOf(ChatColor.COLOR_CHAR));
+    }
+
+    private static boolean isDecorationLetter(String letter) {
         String regex = "(?i)[0-9A-FK-ORX]";
-        return str0.equals(String.valueOf(ChatColor.COLOR_CHAR))
-                && str1.matches(regex);
+        return letter.matches(regex);
     }
 }
