b3da4b28116fee39a7931267fad5cecdd212b520
kidocchy <kidocchy@localhost>
2022-06-18 08:53:49 +0900

    for device sync

--- src/main/java/tech/inudev/profundus/utils/PFID.java
+++ src/main/java/tech/inudev/profundus/utils/PFID.java
@@ -1,20 +1,49 @@
 package tech.inudev.profundus.utils;
 
-import java.util.UUID;
+import java.util.*;
 
 
 public class PFID {
+	
+	private static Map<UUID, UUID> storedPFIDList = new HashMap<UUID,UUID>();
+	
 	/**
 	 *Convert Minecraft Player UUID to Profundus user UUID;
 	 *This may enable secure management of transactions;
 	 *
-	 * @param uuid
-	 * @return uuid
+	 * @param uuid Minecraft Player UUID
+	 * @return uuid Profundus User UUID
 	 */
 	static UUID getPFID(UUID uuid) {
-		//search DB for PFID that matches Player UUID;
-		//If none, create random UUID;
+		UUID yourID = newUUID(); //とりあえず，仮に発行しておく。
+		//
+		if(storedPFIDList.containsKey(uuid)) {
+			yourID = storedPFIDList.get(uuid); //HashMapにあれば，それを返却。
+			return yourID;
+		}
+		if(true) { 
+			//search DB for PFID that matches Player UUID;
+			//yourID = *getFromDB*
+		} else {
+				//add new entry to DB (e.g. new user)
+		}
+		storedPFIDList.put(uuid, yourID);
+		//add to class variables(HashMap) for optimization
 		
+		return yourID;
+	}
+	
+	static UUID newUUID() {
+		//とりあえず，ランダムで発行。
 		return UUID.randomUUID();
 	}
+	
+	static void removeFromStoredList(UUID uuid) {
+		if(storedPFIDList.containsKey(uuid)) {
+			storedPFIDList.remove(uuid);
+		}
+	}
+	
+	
+	
 }
--- src/main/java/tech/inudev/profundus/utils/User.java
+++ src/main/java/tech/inudev/profundus/utils/User.java
@@ -1,16 +1,96 @@
 package tech.inudev.profundus.utils;
 
 import org.bukkit.entity.*;
-import java.util.UUID;
+import java.util.*;
+import java.time.*;
 
 public class User {
+	private static Map<UUID,User> storedList = new HashMap<UUID,User>();
 	private String screenName;
 	private UUID uuid;
 	private UUID pfid;
+	private Instant memberSince;
+	private Instant lastLogin;
+	private String mainLanguage;
+	private String subLanguage;
+	private Boolean isPrincipal;
 	
-	User(Player player) {
+	private User(Player player) {
 		uuid = player.getUniqueId();
 		pfid = PFID.getPFID(uuid);
+		screenName = player.getName();
+		memberSince = java.time.Instant.now();
+		lastLogin = java.time.Instant.now();
+	}
+	
+	public User getUser(Player player) {
+		return getUser(player, true);
+	}
+	
+	public User getUser(Player player,Boolean createIfNotExist) {
+		UUID queryUUID = player.getUniqueId();
+		User user;
+		if(storedList.containsKey(queryUUID)) {
+			user = storedList.get(queryUUID);
+		}else if(isExistsOnDB(queryUUID)) {
+			//searchDBforExistingUser
+			user = fetch(queryUUID);
+			user.addToStoredList();
+		}else {
+			if(!createIfNotExist) {return null;}
+			user = new User(player);
+			user.addToDB();
+			user.addToStoredList();
+		}
+		return user;
+	}
+	
+	private Boolean isExistsOnDB(UUID q) {
+		return false;
+	}
+	
+	private User fetch(UUID q) {
+		return null;
+	}
+	
+	private void addToDB() {
 		
 	}
+	
+	private void updateDB() {
+		
+	}
+	
+	private void removeFromDB() {
+		
+	}
+	
+	private void addToStoredList() {
+		storedList.put(uuid, this);
+	}
+	
+	void removeFromStoredList() {
+		storedList.remove(uuid);
+	}
+	
+	public void updateLastLogin() {
+		lastLogin = java.time.Instant.now();
+		updateDB();
+	}
+	
+	public void setPrinicipal(Boolean set) {
+		isPrincipal = set;
+		updateDB();
+	}
+	
+	public void setMainLanguage(String lang) {
+		mainLanguage = lang;
+		updateDB();
+	}
+	
+	public void setSubLanguage(String lang) {
+		subLanguage = lang;
+		updateDB();
+	}
+	
 }
