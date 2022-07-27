dee02a931f49cba452429625a01e93b16c645976
kumitatepazuru <43287589+kumitatepazuru@users.noreply.github.com>
2022-07-27 16:41:28 +0900

    Delete docs directory

--- docs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"tech.inudev.profundus"},{"l":"tech.inudev.profundus.config"},{"l":"tech.inudev.profundus.define"},{"l":"tech.inudev.profundus.listener"},{"l":"tech.inudev.profundus.scheduler"},{"l":"tech.inudev.profundus.utils"}];updateSearchResults();
